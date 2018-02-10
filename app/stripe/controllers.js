23687f32f54dc3cc1581e911ac54c7bf3c6de663
phytertek <ryan.phytertek@gmail.com>
2018-02-10 16:05:59 -0500

    stripe func peek 3

--- app/stripe/controllers.js
+++ app/stripe/controllers.js
@@ -85,7 +85,7 @@ module.exports = {
   },
   getStripePeekAuth: async (req, res) => {
     try {
-      console.log(strip);
+      console.log(stripe);
       res.json(stripe);
     } catch (error) {
       sendUserError(error, res);
